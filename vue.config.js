f0ffd1449df76a3bf75af342267db8784745c91b
anitahuang121 <43851538+anitahuang121@users.noreply.github.com>
2021-07-26 16:00:53 +0800

    test for web

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,14 @@ function resolveSrc(_path) {
 }
 // vue.config.js
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/eric-project/'
+    : '/'
+}
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tsmc-project/'
+    : '/'
   lintOnSave: true,
   configureWebpack: {
     // Set up all the aliases we use in our app.
