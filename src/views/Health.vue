1fc3e58e61de8b2c975b0e8dbb44b009b78dfcd4
SNCaitlynY <74636955+SNCaitlynY@users.noreply.github.com>
2021-08-05 21:02:16 +0800

    Update Health.vue

--- src/views/Health.vue
+++ src/views/Health.vue
@@ -17,76 +17,22 @@
             </b-container>
         </b-container>
         </div>
-            
-            <b-card-group deck>
-                <b-card class="mt-3" header="已施打疫苗名單" header-bg-variant="info" header-text-variant="white">
-                    <b-card-text>
-                    每天於疫苗施打完畢後 (17:00)，更新已施打疫苗的員工資料。
-                    </b-card-text>
+            <b-card class="mt-3" header="已施打疫苗名單" header-bg-variant="info" header-text-variant="white">
+                <b-card-text>
+                每天於疫苗施打完畢後 (17:00)，更新已施打疫苗的員工資料。
+                </b-card-text>
 
-                    <b-card-text>若需修改或新增，請使用右方手動更改。</b-card-text>
+                <b-card-text>請勾選施打狀態。</b-card-text>
 
 
-                    <b-table striped hover :items="items" :fields="fields"></b-table>
+                <b-table striped hover :items="items" :fields="fields"></b-table>
+                <b-form-checkbox>已施打</b-form-checkbox>
 
 
-
-                    <a href="#" class="card-link">Card link</a>
-                    <b-link href="#" class="card-link">Another link</b-link>
-                </b-card>
-
-                <b-card class="mt-3" header="增加已施打疫苗員工" header-bg-variant="warning" header-text-variant="white">
-                    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-                        <b-form-group
-                        id="input-group-1"
-                        label="Employee ID:"
-                        label-for="input-1"
-                        description="We'll never share your information with anyone else."
-                        >
-                        <b-form-input
-                            id="input-1"
-                            v-model="form.employeeID"
-                            type="ID"
-                            placeholder="Enter employee ID"
-                            required
-                        ></b-form-input>
-                        </b-form-group>
-
-
-
-                        <b-form-group id="input-group-3" label="Vaccine Type:" label-for="input-3">
-                        <b-form-select
-                            id="input-3"
-                            v-model="form.vaccine"
-                            :options="vaccine"
-                            required
-                        ></b-form-select>
-                        </b-form-group>
-
-                        <b-form-group
-                        id="input-group-1"
-                        label="Date:"
-                        label-for="input-1"
-                        description="Please follow the rule: YYYY-MM-DD"
-                        >
-                        <b-form-input
-                            id="input-1"
-                            v-model="form.date"
-                            type="ID"
-                            placeholder="Date"
-                            required
-                        ></b-form-input>
-                        </b-form-group>
-
-                        <b-button type="submit" variant="primary">Submit</b-button>
-                        <b-button type="reset" variant="danger">Reset</b-button>
-                    </b-form>
-                    <b-card class="mt-3" header="Form Data Result">
-                        <pre class="m-0">{{ form }}</pre>
-                    </b-card>
-                </b-card>
-            </b-card-group>
-    </div>
+                <a href="#" class="card-link">Card link</a>
+                <b-link href="#" class="card-link">Another link</b-link>
+            </b-card>
+        </div>
 </template>
 
 <script>
