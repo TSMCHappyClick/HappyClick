313731f3ae62d4cd48ae83b95989876dd7b75b72
anitahuang121 <210503anita@gmail.com>
2021-08-08 10:29:08 +0800

    check page api finished

--- src/views/Check.vue
+++ src/views/Check.vue
@@ -40,13 +40,10 @@
                         >
                           您的預約資料已刪除，請重新預約! {{ dismissCountDown }} 
                         </b-alert>
-                        <b-button @click="showAlert" variant="info" class="m-1" >
+                        <b-button @click="myconfirm" variant="info" class="m-1" >
                           刪除預約資料
                         </b-button>
                     </b-card-text>
-
-
-                   
 
                 </b-card>
 
@@ -55,24 +52,48 @@
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     data() {
       return {
-        fields: ['employeeID', 'name', 'department','vaccine','date'],
+        fields: ['employeeID', 'vaccine_type','date'],
         items:[
-            {employeeID: '120332' ,name: 'Anson',department: 'TSID',vaccine: 'AstraZeneca',
-            date:'2021-08-05'},],
+            {employeeID: localStorage.ID ,vaccine_type: 'QQ', date:''}],
         dismissSecs: 3,
         dismissCountDown: 0
       }
     },
+    created() {
+        axios
+          .post('https://happyclick-healthcenter.herokuapp.com/checkReserve', {"ID": localStorage.ID})
+          .then(res => {
+            console.log(res.data);
+            console.log(res.data.date);
+            console.log(res.data.vaccine_type);
+            console.log(this.items[0].vaccine_type);
+            this.items[0].vaccine_type = res.data.vaccine_type;
+            this.items[0].date = res.data.date;
+          })
+          .catch( (error) => console.log(error));
+    },
     methods: {
+      myconfirm () {
+        if(confirm('確定要刪除嗎? 刪除後不可恢復 只能重新預約~')==true){
+          this.handleDelete()
+        }
+      },
+      handleDelete() {
+        const reservedData = {"ID": localStorage.ID, "date": this.items[0].date, "vaccine_type": this.items[0].vaccine_type};
+        axios
+          .post('https://happyclick-healthcenter.herokuapp.com/removeReserve', reservedData)
+          .then(res => {
+            alert(res.data.msg);
+            this.$router.push('/reserve');
+          })
+          .catch( (error) => console.log(error));
+      },
       countDownChanged(dismissCountDown) {
         this.dismissCountDown = dismissCountDown
-      },
-      showAlert() {
-        alert("您的預約將被刪除!");
-        this.dismissCountDown = this.dismissSecs
       }
     }
   }
