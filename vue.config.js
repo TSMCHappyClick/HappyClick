3e3d177313e2f98b7663fa5b6c4517cfadd11395
anitahuang121 <210503anita@gmail.com>
2021-08-10 21:14:19 +0800

    for deploy on heroku

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ function resolveSrc(_path) {
 }
 module.exports = {
   publicPath: '/HappyClick/',
+  outputDir: 'dist/HappyClick/',
   lintOnSave: true,
   configureWebpack: {
     // Set up all the aliases we use in our app.
